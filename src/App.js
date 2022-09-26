91f3228f8656e394c77f75ca7493379452e9e2f8
mPemba <michael.j.say@gmail.com>
2022-09-26 11:41:40 -0600

    starting to form a decent design

--- src/App.js
+++ src/App.js
@@ -7,27 +7,41 @@ const App = () => {
     <Main>
       <Content>
         <Title>
-          Hello my name is Michael Sayer.
+          Michael Sayer
         </Title>
-        <Title>I am a musician in Salt Lake City, UT. </Title>
-        <Title>I have multiple projects including </Title>
-        <Title>music for film and television.</Title>
-        <div>
+        <SubTitle>Composer based in Salt Lake City</SubTitle>
+        <SubTitle>I have multiple projects under way</SubTitle>
+        <SubTitle>including music for film and television.</SubTitle>
+        <ColorsVertical>
           <div style={{ width: '25px', height: '25px', background: '#E8EEF2' }}></div>
           <div style={{ width: '25px', height: '25px', background: '#D6C9C9' }}></div>
           <div style={{ width: '25px', height: '25px', background: '#C7D3DD' }}></div>
           <div style={{ width: '25px', height: '25px', background: '#77B6EA' }}></div>
           <div style={{ width: '25px', height: '25px', background: '#37393A' }}></div>
-        </div>
-        <Links>
-          <LinkBox>
-            <SubTitle>Taxi:</SubTitle>
-            <Link href="https://www.taxi.com/members/michael-sayer" target="_blank" rel="noopener noreferrer">
-              Taxi Member Profile
-            </Link>
-          </LinkBox>
+        </ColorsVertical>
+        <Projects>
+          <Project>
+            <Details>
+              <div>
+                <SubTitle>music for film</SubTitle>
+                <Text>Proud Taxi Member - Not placed yet but my music has been signed to multiple libraries.</Text>
+                <Link href="https://www.taxi.com/members/michael-sayer" target="_blank" rel="noopener noreferrer">
+                  Taxi Member Profile
+                </Link>
+              </div>
+              <Spacer>
+                <ColorsHorizontal>
+                  <div style={{ width: '20%', height: '25px', background: '#E8EEF2' }}></div>
+                  <div style={{ width: '20%', height: '25px', background: '#D6C9C9' }}></div>
+                  <div style={{ width: '20%', height: '25px', background: '#C7D3DD' }}></div>
+                  <div style={{ width: '20%', height: '25px', background: '#77B6EA' }}></div>
+                  <div style={{ width: '20%', height: '25px', background: '#37393A' }}></div>
+                </ColorsHorizontal>
+              </Spacer>
+            </Details>
+          </Project>
 
-          <LinkBox>
+          <Project>
             <SubTitle>Ghost of Young Michael:</SubTitle>
             <Link href="https://youngmichael.bandcamp.com/" target="_blank" rel="noopener noreferrer">
               bandcamp
@@ -35,15 +49,15 @@ const App = () => {
             <Link href="youtube.com/user/michaeljsay" target="_blank" rel="noopener noreferrer">
               youtube
             </Link>
-          </LinkBox>
+          </Project>
 
-          <LinkBox>
+          <Project>
             <SubTitle>The Great Turtle:</SubTitle>
             <Link href="https://greatturtle.bandcamp.com/" target="_blank" rel="noopener noreferrer">
               bandcamp
             </Link>
-          </LinkBox>
-        </Links>
+          </Project>
+        </Projects>
       </Content>
     </Main>
   );
@@ -53,16 +67,17 @@ const Main = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   background: ${neveColors.darkBlue};
 `;
 
 const Content = styled.div`
   width: 100%;
-  height: 72%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   background: ${neveColors.darkBlue};
-  padding: 20px;
+  padding: 64px 44px;
 
   @media (max-width: 768px) {
     width: 90%;
@@ -75,27 +90,62 @@ const Content = styled.div`
 `;
 
 const Title = styled.h1`
-  font-size: calc(48px + (20 * (100vw - 1024px)) / 416);
-  margin: 0px;
+  font-size: calc(42px + (20 * (100vw - 1024px)) / 416);
+  margin: 0px 0px 20px 0px;
   color: ${neveColors.white};
 `;
 
 const SubTitle = styled.h1`
-  font-size: 2em;
+  font-size: calc(30px + (20 * (100vw - 1024px)) / 416);
   margin: 0px;
   color: ${neveColors.white};
 `;
 
-const Links = styled.div`
+const ColorsVertical = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 20px;
 `;
 
-const LinkBox = styled.div`
+const ColorsHorizontal = styled.div`
   display: flex;
   flex-direction: row;
+  margin-top: 20px;
+`;
+
+const Projects = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+`;
+
+const Project = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
+  margin-bottom: 44px;
+`;
+
+const Details = styled.div`
+  width: 100%;
+  display: flex;
+  margin-left: 10px;
+`;
+
+const Spacer = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Text = styled.p`
+  font-size: calc(20px + (20 * (100vw - 1024px)) / 416);
+  margin: 0px;
+  color: ${neveColors.white};
 `;
 
 const Link = styled.a`
