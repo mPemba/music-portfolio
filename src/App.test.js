6e861e5fa3d7c6cd7f2cf393c7c766c2354428b5
mPemba <michael.j.say@gmail.com>
2022-10-05 13:41:47 -0600

    updating test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders my name', () => {
+test('renders sub text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Michael Sayer/i);
+  const linkElement = screen.getByText(/Composer based in Salt Lake City./i);
   expect(linkElement).toBeInTheDocument();
 });
