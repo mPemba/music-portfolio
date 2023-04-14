04e85daa7bcf412c1952d738e2757cd1702eabce
mPemba <michael.j.say@gmail.com>
2023-04-14 16:04:38 -0600

    breaking out components, styling updates, manifest updates

---
+++ src/components/Projects.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors } from '../library/colors';
+import { info } from '../library/info';
+
+const Projects = () => {
+  return (
+    <>
+      <SubTitle>Projects</SubTitle>
+        <ProjectBox>
+          {info.map((item, index) => (
+            <Project key={index}>
+              <Details>
+                <ProjectTitle href={item.link} target="_blank" rel="noopener noreferrer">{item.title}</ProjectTitle>
+                <Text>
+                  {item.text1}
+                </Text>
+                {item.text2 && (
+                  <Text>
+                    {item.text2}
+                  </Text>
+                )}
+              </Details>
+            </Project>
+          ))}
+      </ProjectBox>
+    </>
+  );
+};
+
+const ProjectBox = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+`;
+
+const Project = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 44px;
+
+  @media (max-width: 768px) {
+    justify-content: center;
+  }
+`;
+
+const SubTitle = styled.h2`
+  font-size: clamp(2rem, 5vw, 3rem);
+  line-height: 44px;
+  margin: 0px;
+  color: ${colors.textBlack};
+
+  @media (max-width: 768px) {
+    line-height: 20px;
+  }
+`;
+
+const Details = styled.div`
+  width: 42%;
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 768px) {
+    width: 82%;
+  }
+`;
+
+const ProjectTitle = styled.a`
+  width: fit-content;
+  font-size: 22px;
+  color: ${colors.textBlack};
+  margin: 0px 0px 19px 0px;
+  text-decoration: none;
+  line-height: 18px;
+  :hover {
+    cursor: pointer;
+    color: ${colors.blue}
+  }
+`;
+
+const Text = styled.p`
+  max-width: 420px;
+  font-size: 16px;
+  font-family: 'Hanken Grotesk';
+  margin: 0px 0px 19px 0px;
+  line-height: 1.5;
+  color: ${colors.textWhite};
+`;
+
+
+export { Projects };
---
+++ src/library/info.js
@@ -0,0 +1,46 @@
+const info = [
+  {
+    title: 'Music for Film',
+    link: 'https://www.taxi.com/members/michael-sayer',
+    text1: 'A proud Taxi member for years. I love the process and the challenge of writing music for film.',
+    text2: 'My music has been featured in indie films and podcasts and I\'m always looking for new projects.'
+  },
+  {
+    title: 'Ghost of Young Michael',
+    link: 'https://youngmichael.bandcamp.com',
+    text1: 'Ghost of Young Michael is an ambient solo project. Using acoustic instruments and electric processing.',
+    text2: 'I\'m exploring the relationship between the natural and the artificial.'
+  },
+  {
+    title: 'Great Turtle',
+    link: 'https://greatturtle.bandcamp.com',
+    text1: 'The Great Turtle is an electro-acoustic duo formed with Casey Dorcey. Sound exploration and organic development are at the heart of the music.',
+    text2: 'Blending native/folk like melodies with huge atmospheric world drones, The Great Turtle seeks to take their listeners on the ecstatic journey.',
+  },
+  {
+    title: 'Crime Tiger',
+    link: 'https://crimetiger.bandcamp.com',
+    text1: 'Crime Tiger is a rock \'n roll band. We are currently recording our first album. We are looking forward to sharing it with you soon.',
+    text2: '',
+  }
+];
+
+const links = [
+  {
+    link: 'https://www.linkedin.com/in/mikesayer/',
+    logo: '/linkedin.svg',
+    alt: 'linkedin',
+  },
+  {
+    link: 'https://www.twitter.com/michaeljsayer/',
+    logo: '/twitter.svg',
+    alt: 'twitter',
+  },
+  {
+    link: 'https://www.michaelsayer.com/',
+    logo: '/code-solid.svg',
+    alt: 'software engineering cv',
+  },
+];
+
+export { info, links };
